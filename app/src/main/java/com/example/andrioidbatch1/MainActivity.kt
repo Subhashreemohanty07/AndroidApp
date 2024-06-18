9210f1522ee8ec84d9f847c12db9f884bd6f6f19
Subhashreemohanty07 <subha.mohanty2003@gmail.com>
2024-06-18 20:30:15 +0530

    use the marquee

--- app/src/main/java/com/example/andrioidbatch1/MainActivity.kt
+++ app/src/main/java/com/example/andrioidbatch1/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.basicMarquee
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,22 +18,24 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.andrioidbatch1.ui.theme.AndrioidBatch1Theme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         setContent {
-            //for gradient of color we have to make a list
-            val gColor= listOf(Color.Red,Color.Green,Color.Red)
-            Text(text = "Hello World", fontSize = 40.sp,
-               // color = Color.Green,
-                style = TextStyle(brush = Brush.linearGradient(
-                    colors = gColor
-                ))
-            )
+          Column (modifier = Modifier.width(400.dp)){
+              Text(text = "Hello World I am Subhashre Mohanty and Studing",
+                  modifier =Modifier.basicMarquee(),
+                  fontSize = 50.sp,
+                  color = Color.Blue
+              )
+          }
+
         }
     }
 }
