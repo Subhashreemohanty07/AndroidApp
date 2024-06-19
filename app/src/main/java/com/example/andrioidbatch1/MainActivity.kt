004f2eb115b63de7405addcc6d4a76ca2ffa9488
Subhashreemohanty07 <subha.mohanty2003@gmail.com>
2024-06-19 11:51:39 +0530

    use different modifiers

--- app/src/main/java/com/example/andrioidbatch1/MainActivity.kt
+++ app/src/main/java/com/example/andrioidbatch1/MainActivity.kt
@@ -13,11 +13,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -33,21 +35,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         setContent {
-//use different modifiers
-            Text(text = "Hello World",
-                fontSize = 50.sp,
-                modifier = Modifier
-                    .padding(20.dp)
-                    .background(Color.Green)
-                    .clickable {
-                        println("I am clicked")
-                    }
-                    .border(4.dp, Color.Red)
-                    .padding(7.dp)
-                
+//use alignment
+            Row(modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically)
+            {
+
+                Text(text = "Hello World",
+                    fontSize = 50.sp,
+                    modifier = Modifier
+                        .padding(20.dp)
+                        .background(Color.Green)
+                        .clickable {
+                            println("I am clicked")
+                        }
+//                        .border(4.dp, Color.Red)
+//                        .padding(7.dp)
+
+
                 )
 
-
+            }
 
         }
     }
