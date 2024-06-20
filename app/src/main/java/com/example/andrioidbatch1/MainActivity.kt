7861a85cad39c98f71fef3a0fd6e6fff14f0c514
Subhashreemohanty07 <subha.mohanty2003@gmail.com>
2024-06-21 01:31:23 +0530

    Progress Indicators

--- app/src/main/java/com/example/andrioidbatch1/MainActivity.kt
+++ app/src/main/java/com/example/andrioidbatch1/MainActivity.kt
@@ -14,7 +14,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
@@ -27,10 +29,12 @@ import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
@@ -39,9 +43,9 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Horizontal
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -52,6 +56,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.andrioidbatch1.ui.theme.AndrioidBatch1Theme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+import androidx.compose.ui.Alignment.Companion as Alignment
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
@@ -59,39 +65,33 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         setContent {
-            //use scaffold
-//            Scaffold(
-//                topBar = Top
-Column {
-    CardExample()
-    ChipExample()
-}
-//            )
+            Text(text = "Progress Indicator", fontSize = 30.sp)
+            ProgressIndExample()
         }
     }
 @OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
- @Composable
-        fun ScafflodSample() {}
 @Composable
-fun CardExample(){
-    Card (
-        colors = CardDefaults.cardColors(containerColor = Color.Green
-        ), modifier = Modifier
-            .size(width = 300.dp, height = 100.dp)
-            .padding(10.dp),
-    ){
-        Text(text = "Simple Card Example",
-            modifier = Modifier.padding(20.dp), textAlign = TextAlign.Center)
+    fun ProgressIndExample() {
+    Column (horizontalAlignment = Alignment.CenterHorizontally,
+        modifier= Modifier
+            .fillMaxSize()
+            .fillMaxWidth()
+            .padding(20.dp))
+        {
+        LinearProgressIndicator(progress = 0.8f,
+            modifier = Modifier
+                .padding(50.dp)
+                .height(15.dp)
+                .fillMaxWidth()
+                .background(Color.Gray), color = Color.Blue,
+            )
+            CircularProgressIndicator(modifier = Modifier.size(150.dp),
+                color = Color.Blue,
+                strokeWidth = 10.dp,
+                progress = 0.6f)
+        }
+
     }
-}@Composable
-    fun ChipExample(){
-        AssistChip(onClick = { /*TODO*/ }, label = { Text(text = "I am an AssistChip") },
-            leadingIcon ={
-                Icon(Icons.Filled.Menu,
-                    contentDescription = "")
-            })}
-
-
     }
 
 
