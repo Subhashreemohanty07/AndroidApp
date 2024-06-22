7d4a062e370b5572989c8d27467905d88a5f3437
Subhashreemohanty07 <subha.mohanty2003@gmail.com>
2024-06-23 01:31:15 +0530

    Api calls

--- app/src/main/java/com/example/andrioidbatch1/MainActivity.kt
+++ app/src/main/java/com/example/andrioidbatch1/MainActivity.kt
@@ -3,139 +3,135 @@ package com.example.andrioidbatch1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.basicMarquee
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.paddingFromBaseline
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.AlertDialogDefaults
-import androidx.compose.material3.AssistChip
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.LinearProgressIndicator
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Slider
-import androidx.compose.material3.Switch
-import androidx.compose.material3.SwitchDefaults
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment.Horizontal
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.andrioidbatch1.ui.theme.AndrioidBatch1Theme
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
-import androidx.compose.ui.Alignment.Companion as Alignment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.*
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.POST
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
+    @OptIn(ExperimentalFoundationApi::class)
+    private val productVM: ProductViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         setContent {
-            Text(text = "Switch", fontSize = 40.sp)
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize(),
-                    contentAlignment = Alignment.Center
-                ){
-
-                    SwitchExample()
+            val products by productVM.products.observeAsState(emptyList())
+            Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                LazyColumn {
+                    items(products) {product ->
+                        ProductItem(product = product)
+                    }
                 }
+            }
         }
     }
-@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
-@Composable
-fun SwitchExample(){
-    var isChecked by remember {
-        mutableStateOf(value = false)
-    }
-    var  icons= if (isChecked) Icons.Filled.Check  else Icons.Filled.Close
-    Switch(checked = isChecked, onCheckedChange ={ isChecked=it },
-        thumbContent = {
-            Icon(
-                imageVector =icons,
-                contentDescription = null,
-                modifier = Modifier.size(SwitchDefaults.IconSize)
-            )
-        })
-}
-@Composable
-fun SliderExample()
-{
-    var  sliderPosition by remember {
-        mutableStateOf(value= 0f)
-    }
-    Column(modifier = Modifier.padding(10.dp),
-        horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = sliderPosition.toString())
-        Slider(value = sliderPosition, onValueChange ={sliderPosition=it} )
-    }
 
-}
     @Composable
-    fun ProgressIndExample() {
-    Column (horizontalAlignment = Alignment.CenterHorizontally,
-        modifier= Modifier
-            .fillMaxSize()
-            .fillMaxWidth()
-            .padding(20.dp))
-        {
-        LinearProgressIndicator(progress = 0.8f,
+    fun ProductItem(product: Product) {
+        Card(
             modifier = Modifier
-                .padding(50.dp)
-                .height(15.dp)
                 .fillMaxWidth()
-                .background(Color.Gray), color = Color.Blue,
-            )
-            CircularProgressIndicator(modifier = Modifier.size(150.dp),
-                color = Color.Blue,
-                strokeWidth = 10.dp,
-                progress = 0.6f)
+                .padding(10.dp)
+        ) {
+            Column(
+                modifier = Modifier.padding(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = product.title, color = Color.Red)
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(text = product.category)
+            }
         }
+    }
+}
 
+
+//Model Class
+data class  Product(
+    val id: Int,
+    val title: String,
+    val price: Double,
+    val description: String,
+    val category: String,
+    val image: String,
+    val rating: Rating
+)
+
+data class Rating(
+    val rate: Float,
+    val count: Int
+)
+
+interface  ApiService {
+    @GET("products")
+    suspend fun getProducts(): List<Product>
+}
+
+object RetrofitClient {
+    private const val BASE_URL = "https://fakestoreapi.com/"
+
+    val  apiService: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+}
+
+class ProductRepository(private val apiService: ApiService) {
+    suspend fun getProducts(): List<Product> {
+        return apiService.getProducts()
+    }
+}
+
+class ProductViewModel: ViewModel() {
+    private  val _products = MutableLiveData<List<Product>>()
+    val products: LiveData<List<Product>> get() = _products
+
+    private  val repository = ProductRepository(RetrofitClient.apiService)
+    init {
+        fetchProducts()
     }
 
+    fun fetchProducts() {
+        viewModelScope.launch {
+            try {
+                val productList = repository.getProducts()
+                _products.postValue(productList)
+                println(productList)
+
+            } catch (e: Exception) {
+
+            }
+        }
     }
+}
+
+
 
 
 
